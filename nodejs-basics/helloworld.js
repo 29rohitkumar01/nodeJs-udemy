a2eee3801de0842bdc2346024b449ddbd15899dc
29rohitkumar01 <29rohitkumar01@gmail.com>
2021-02-05 21:03:56 +0530

    Comments for Running the command added

--- nodejs-basics/helloworld.js
+++ nodejs-basics/helloworld.js
@@ -1,1 +1,4 @@
+// To run this program in the terminal
+// First come to this directory nodejs-basics in terminal
+// Then type: node helloworld.js
 console.log('Hello Rohit! First course on Nodejs.')
