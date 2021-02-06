91f52126b7a5b752d010682a28f57bf430e69e58
(29rohitkumar01) <(29rohitkumar01@gmail.com)>
2021-02-06 19:12:48 +0530

    Lecture 12: objects assignment modifications

--- nodejs-basics/lec12_objects.js
+++ nodejs-basics/lec12_objects.js
@@ -5,10 +5,8 @@ const myPet = {
 }
 
 function printPetDetails(pet = {}){
-  if(pet.type === 'Dog'){
-    console.log('You own a Dog named ' + pet.name)
-  } else if(pet.type === 'Cat'){
-    console.log('You own a cat named ' + pet.name)
+  if(pet.type === 'Dog' || pet.type === 'Cat'){
+    console.log('You own a ' + pet['type'] + ' named ' + pet.name)
   } else {
     console.log("You don't own a cat or dog as pet. Get one soon!!")
   }
