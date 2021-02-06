5ce5fea4e518e5f88aea79000e9d5a1b437867aa
29rohitkumar01 <29rohitkumar01@gmail.com>
2021-02-06 16:31:55 +0530

    Lecture 9: Numbers

---
+++ nodejs-basics/lec9_numbers.js
@@ -0,0 +1,8 @@
+var score1 = 96;
+var score2 = 92;
+var totalScore = score1 + score2;
+var averageScore = totalScore/2;
+console.log('score 1: ', score1);
+console.log('score 2: ', score2);
+console.log('average:', averageScore);
+console.log('percentage:',(totalScore/200)*100+'%' )
