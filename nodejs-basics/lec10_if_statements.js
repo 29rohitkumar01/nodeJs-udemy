d6f9ed1bbfba534dc6b11780164d74f3aa14bcba
rohit kumar <rohit.kumar@innoplexus.com>
2021-02-06 17:01:23 +0530

    Lecture 10: If statements

---
+++ nodejs-basics/lec10_if_statements.js
@@ -0,0 +1,19 @@
+var friend1 = 'Jack';
+var friend2 = 'Michael';
+var friend3 = 'Andrew';
+var friend4 = 'Shakeel';
+var friend5 = 'Brad';
+var visitor = 'Rohit'  // change this variable
+if(visitor === friend1){
+    console.log('Welcome ' + friend1);
+}else if(visitor === friend2){
+    console.log('Hey ' + friend2);
+}else if(visitor === friend3){
+    console.log('Adios Amigos ' + friend3);
+}else if(visitor === friend4){
+    console.log('Namaskaar ' + friend4);
+} else if(visitor === friend5){
+    console.log('Gracias '+ friend5)
+} else {
+    console.log('Who are you stranger!!')
+}
