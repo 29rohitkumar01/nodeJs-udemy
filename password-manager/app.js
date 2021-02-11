d41e069ac9cfc96d8fafce2fb37356feeb10693c
(29rohitkumar01) <(29rohitkumar01@gmail.com)>
2021-02-11 15:15:06 +0530

    Lecture 26: Project password manager learning and assignment

--- password-manager/app.js
+++ password-manager/app.js
@@ -12,24 +12,86 @@ console.log('Starting password manager')
 // const name = storage.getItemSync('name');
 // console.log('name is: ', name)
 
-
+// <-----------------LECTURE 25 STARTS ------------>
 // Example 2:
 // no need to import twice and start storage again, done to get into practice
-var storage = require('node-persist');
-storage.initSync()
-storage.setItemSync('accounts', [{
-    name: 'Rohit Kumar',
-    date: '11 Feb 2021'
-}])
-const accountsData = storage.getItemSync('accounts')
-console.log('accounts Data: ', accountsData)
+// var storage = require('node-persist');
+// storage.initSync()
+// storage.setItemSync('accounts', [{
+//     name: 'Rohit Kumar',
+//     date: '11 Feb 2021'
+// }])
+// const accountsData = storage.getItemSync('accounts')
+// console.log('accounts Data: ', accountsData)
 
 
 // Assignment: Push new account
-accountsData.push({
-    name: 'Albert Dendam',
-    date: '11 Feb 2021'
-})
+// accountsData.push({
+//     name: 'Albert Dendam',
+//     date: '11 Feb 2021'
+// })
 
-storage.setItemSync('accounts', accountsData);
-console.log('updated accounts', storage.getItemSync('accounts'));
+// storage.setItemSync('accounts', accountsData);
+// console.log('updated accounts', storage.getItemSync('accounts'));
+// <------------------LECTURE 25 ENDS ---------------->
+
+
+
+
+// <------------------LECTURE 26 STARTS ---------------->
+console.log('<------------------LECTURE 26 STARTS ---------------->')
+
+var storage = require('node-persist');
+storage.initSync();
+
+function createAccount(account){
+    console.log('Account creation starts!!')
+    let storedAccounts = storage.getItemSync('stored_accounts');
+    if(typeof storedAccounts === 'undefined'){
+      storedAccounts = []
+    }
+    storedAccounts.push(account);
+    storage.setItemSync('stored_accounts', storedAccounts);
+    console.log('Account created: ', account);
+    return account
+}
+
+function getAccount(accountName){
+    let storedAccounts = storage.getItemSync('stored_accounts');
+    if(typeof storedAccounts === 'undefined'){
+        console.log('No account found. Please create account first !!')
+        storedAccounts = []
+    }
+    let matchedAccount = null
+    for(let i=0; i < storedAccounts.length; i++){
+        if(storedAccounts[i].username === accountName){
+            matchedAccount = storedAccounts[i];
+            break;
+        }
+    }
+    if(matchedAccount === null){
+        console.log('Searched account ' + accountName + ' Not found');
+    } else {
+        console.log('Account found: ', matchedAccount)
+    }
+    return matchedAccount
+}
+
+getAccount();  // for undefined case
+createAccount({
+    username: 'rohitkumar',
+    name: 'Rohit Kumar',
+    date: '20-12-2005',
+    balance: 100000
+})
+getAccount('xyz') //  for account not found case
+createAccount({
+    username: 'andrewShumak',
+    name: 'Andrew Shumak',
+    date: '20-09-2010',
+    balance: 100
+})
+getAccount('rohitkumar');
+
+console.log('<------------------LECTURE 26 ENDS ---------------->')
+// <------------------LECTURE 26 ENDS ---------------->
