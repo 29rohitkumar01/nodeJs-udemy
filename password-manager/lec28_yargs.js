1e4458d4b1d0d7ccaaac9939f8fe2a4ce6ce524a
29rohitkumar01 <29rohitkumar01@gmail.com>
2021-02-13 13:53:00 +0530

    Added comments for lect 28 and use case

--- password-manager/lec28_yargs.js
+++ password-manager/lec28_yargs.js
@@ -1,3 +1,6 @@
+// Come to the directory containing this file
+// To run enter command --> node lec28_vargs.js hello --name ROhit --lastname Kumar
+// also user can use help command --> node node lec28_vargs.js hello --help
 var argv = require('yargs').command(
     'hello',
     'Greets user',
@@ -5,7 +8,7 @@ var argv = require('yargs').command(
         yargs.options({
           name : {
               demand: true,
-              alias: 'n',
+              alias: 'n',  // alias that can be used in place of name so --name / --n
               description: 'Your first name goes here'
           },
           lastname: {
